ebc676446152a9f2b34ec33393b1a021e42abba4
Jeff O'Leary <jeffoleary@server.local.private>
2015-06-13 16:35:11 -0400

    Example Xcode project using Auto Layout

---
+++ autolayout-sample/autolayout-sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayout-sample
+//
+//  Created by Jeff O'Leary on 6/13/15.
+//  Copyright (c) 2015 GDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
